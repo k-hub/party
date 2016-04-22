5560ee86e0285f0416c51d85f04fc39ca108e217
Hackbright Student <info@hackbrightacademy.com>
2016-04-22 12:57:17 -0700

    Added doctests for is_mel and most_and_least_common_type.

--- party.py
+++ party.py
@@ -2,20 +2,47 @@
 
 
 from flask import Flask, session, render_template, request, flash, redirect
-from flask_debugtoolbar import DebugToolbarExtension
+# from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 app.secret_key = "SECRETSECRETSECRET"
 
 
 def is_mel(name, email):
-    """Is this user Mel?"""
+    """Is this user Mel?
+
+        >>> is_mel('Mel Melitpolski', 'mel@ubermelon.com')
+        True
+
+        >>> is_mel('Judith Butler', 'judith@awesome.com')
+        False
+    """
 
     return name == "Mel Melitpolski" or email == "mel@ubermelon.com"
 
 
 def most_and_least_common_type(treats):
-    """Given list of treats, return {most, least} common types."""
+    """Given list of treats, return {most, least} common types.
+
+        >>> treats = [
+            {"type": "dessert"},
+            {"type": "dessert"},
+            {"type": "dessert"},
+            {"type": "drink"},
+            {"type": "drink"},
+            {"type": "appetizer"}
+            ]
+        >>> party.most_and_least_common_type(treats)
+        ('dessert', 'appetizer')
+
+        >>> treats = [
+            {"type": "drink"}
+            ]
+        >>> party.most_and_least_common_type(treats)
+        ('drink', 'drink')
+
+
+    """
 
     types = {}
 
@@ -113,5 +140,5 @@ def rsvp():
 if __name__ == "__main__":
     app.debug = True
     app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
-    DebugToolbarExtension(app)
+    #  DebugToolbarExtension(app)
     app.run()
